4a9c55cea15466f3fd07c70554c37afccb43d6dd
TobyVanBuynder <tobyvbuynder@gmail.com>
2025-12-26 17:33:58 +0100

    Added Theatre implementation with 7 test boxes

--- src/components/Bookshelf.svelte
+++ src/components/Bookshelf.svelte
@@ -1,8 +1,13 @@
-<script>
-  import { Canvas } from '@threlte/core'
-  import Scene from './3d/Scene.svelte'
+<script lang="ts">
+  import { Canvas } from '@threlte/core';
+  import Scene from './3d/Scene.svelte';
+  import { Studio, Theatre } from '@threlte/theatre';
 </script>
 
-<Canvas>
-  <Scene />
+<Studio enabled={true} />
+
+<Canvas shadows={false}>
+  <Theatre>
+    <Scene />
+  </Theatre>
 </Canvas>
