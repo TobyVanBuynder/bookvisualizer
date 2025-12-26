cbdffb029ceae191dff14f4b445a432255cbe7e7
TobyVanBuynder <tobyvbuynder@gmail.com>
2025-12-26 23:30:54 +0100

    Added perspective camera with orbit controls

---
+++ src/components/3d/camera/Camera.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { T } from '@threlte/core';
+    import { OrbitControls } from '@threlte/extras';
+    export let canOrbit = true;
+</script>
+
+<T.PerspectiveCamera
+    position={[5, 5, 5]}
+    oncreate={(camera) => {
+        camera.lookAt(0,0,0)
+    }}
+    makeDefault
+>
+    {#if canOrbit}
+    <OrbitControls />
+    {/if}
+</T.PerspectiveCamera>
