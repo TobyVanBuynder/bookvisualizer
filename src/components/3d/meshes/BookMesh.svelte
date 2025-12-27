b0ca3297b4672398d26ce3fad9429c3306148397
TobyVanBuynder <tobyvbuynder@gmail.com>
2025-12-27 20:41:26 +0100

    Fixed book meshes and scene with dynamic loading

--- src/components/3d/meshes/BookMesh.svelte
+++ src/components/3d/meshes/BookMesh.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
     import { T } from '@threlte/core';
+    import { SheetObject } from '@threlte/theatre';
     import type { AsyncWritable } from '@threlte/core';
     import { Texture } from 'three';
 
+    export let keyName = "book";
     export let xPos = 0;
     export let texture: AsyncWritable<Texture<HTMLImageElement>>;
 </script>
 
-<T.Mesh position.x={xPos}>
-    <T.BoxGeometry args={[0.2, 1.5, 1]} />
-    {#if $texture}
-    <T.MeshBasicMaterial map={$texture} />
-    {/if}
-</T.Mesh>
+<SheetObject key={keyName}>
+    {#snippet children({ Transform, Sync })}
+        <Transform>
+            <T.Mesh position={[xPos, 0, 0]}>
+                <Sync />
+                <T.BoxGeometry args={[0.2, 1.5, 1]} />
+                {#if $texture}
+                <T.MeshBasicMaterial map={$texture} />
+                {/if}
+            </T.Mesh>
+        </Transform>
+    {/snippet}
+</SheetObject>
--- src/components/3d/scenes/BookshelfScene.svelte
+++ src/components/3d/scenes/BookshelfScene.svelte
@@ -1,38 +1,26 @@
 <script lang="ts">
-    import { useLoader } from '@threlte/core'
     import { useTexture } from '@threlte/extras';
-    import { SheetObject } from '@threlte/theatre';
 
-    import Camera from '../camera/Camera.svelte';
-    import BookMesh from '../meshes/BookMesh.svelte'
+    import Camera from '~/components/3d/camera/Camera.svelte';
+    import BookMesh from '~/components/3d/meshes/BookMesh.svelte'
+
+    import { bookshelfData } from '~/utils/books';
 
     // https://openlibrary.org/api/books?bibkeys=ISBN:9780765389220&format=json&jscmd=data
-    const textureURL = 'https://ia800505.us.archive.org/view_archive.php?archive=/35/items/l_covers_0014/l_covers_0014_43.zip&file=0014432013-L.jpg';
 
-    const books = [
-        { textureURL: textureURL },
-        { textureURL: textureURL },
-        { textureURL: textureURL },
-        { textureURL: textureURL },
-    ];
+    const books = bookshelfData;
+    const bookAssetBaseUrl = "/images/books/compressed/";
+    books.forEach((book) => console.log((`${bookAssetBaseUrl}${book.textureURL}`)));
 
     const canOrbit = import.meta.env.DEV;
-
     const padding = 0.1;
 </script>
 
 <Camera canOrbit={canOrbit}/>
 
-<!-- Book 1 -->
-<SheetObject key="books">
-    {#snippet children({ Transform })}
-        <Transform>
-            {#each books as book, index}
-            <BookMesh
-                xPos={0.2 * index + padding * index}
-                texture={useTexture(book.textureURL)}
-            />
-            {/each}
-        </Transform>
-    {/snippet}
-</SheetObject>
+{#each books as book, bookIndex}
+    <BookMesh
+        keyName={`book${bookIndex}`}
+        texture={useTexture(`${bookAssetBaseUrl}${book.textureURL}`)}
+    />
+{/each}
